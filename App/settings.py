32738a556aba75d2069400375ffa3b505fbdc044
Vu Minh <vuhongminh2899@gmail.com>
2021-04-21 16:40:31 +0700

    first commit

--- App/settings.py
+++ App/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,"static")
 ]
+STATIC_ROOT =  os.path.join(BASE_DIR,"static")
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
